e70de70cf2713b0b4951b723af2fa77d7805d2de
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-03 23:27:04 -0700

    d2p1slow

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,26 @@
-pub fn part1(input: &str) -> usize {
-    input.lines().count()
+fn is_invalid(num: &u64) -> bool {
+    let num_str = num.to_string();
+    if num_str.len() % 2 == 0 {
+        num_str
+            .chars()
+            .zip(num_str.chars().skip(num_str.len() / 2))
+            .all(|(a, b)| a == b)
+    } else {
+        false
+    }
+}
+
+pub fn part1(input: &str) -> u64 {
+    input
+        .trim()
+        .split(',')
+        .map(|range| {
+            let (start, end) = range.split_once('-').unwrap();
+            let start = start.parse::<u64>().unwrap();
+            let end = end.parse::<u64>().unwrap();
+            (start..=end).filter(is_invalid).sum::<u64>()
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -14,10 +35,9 @@ mod tests {
         include_str!("../input/2025/day2.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 18700015741);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2025::day01 as day;
+use adventofcode2025::day02 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day1.txt");
+    let input = include_str!("../input/2025/day2.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
