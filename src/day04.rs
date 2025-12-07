86ac8e022666c3fdb6851bc7b602d3e8534fc0de
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-06 23:05:05 -0700

    d4p1

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,63 @@
+use itertools::Itertools;
+
+type Pos = (isize, isize);
+
+struct World {
+    map: Vec<Vec<char>>,
+}
+
+impl World {
+    fn read(input: &str) -> World {
+        let map = input
+            .lines()
+            .map(|line| line.chars().collect_vec())
+            .collect_vec();
+        World { map }
+    }
+    fn get(&self, pos: Pos) -> Option<char> {
+        if let Ok(i) = usize::try_from(pos.0) {
+            if let Ok(j) = usize::try_from(pos.1) {
+                if let Some(map_row) = self.map.get(i) {
+                    return map_row.get(j).copied();
+                }
+            }
+        }
+        None
+    }
+
+    fn width(&self) -> usize {
+        self.map.len()
+    }
+
+    fn height(&self) -> usize {
+        self.map[0].len()
+    }
+
+    fn check_rolls(&self, pos: Pos) -> bool {
+        self.get(pos) == Some('@')
+            && vec![
+                (pos.0, pos.1 + 1),
+                (pos.0, pos.1 - 1),
+                (pos.0 + 1, pos.1),
+                (pos.0 - 1, pos.1),
+                (pos.0 - 1, pos.1 - 1),
+                (pos.0 + 1, pos.1 + 1),
+                (pos.0 + 1, pos.1 - 1),
+                (pos.0 - 1, pos.1 + 1),
+            ]
+            .iter()
+            .filter(|p| self.get(**p) == Some('@'))
+            .count()
+                < 4
+    }
+}
+
 pub fn part1(input: &str) -> usize {
-    input.lines().count()
+    let w = World::read(input);
+    (0..w.height())
+        .flat_map(|i| (0..w.width()).map(move |j| (i, j)))
+        .filter(|(i, j)| w.check_rolls((*i as isize, *j as isize)))
+        .count()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -14,10 +72,9 @@ mod tests {
         include_str!("../input/2025/day2.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 1533);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2025::day03 as day;
+use adventofcode2025::day04 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day3.txt");
+    let input = include_str!("../input/2025/day4.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
