a6396d3dcff99be2cbfab15bc4afd9b84fbd4195
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-06 22:40:25 -0700

    d3p2

--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,7 @@
+use std::{collections::BTreeSet, fmt::Pointer};
+
+use itertools::Itertools;
+
 pub fn part1(input: &str) -> u32 {
     input
         .lines()
@@ -21,8 +25,25 @@ pub fn part1(input: &str) -> u32 {
         .sum()
 }
 
-pub fn part2(input: &str) -> usize {
-    input.lines().count()
+fn max_joltage(bank: &[u32], length: usize) -> u64 {
+    if length == 0 {
+        return 0;
+    }
+    let max = bank[..bank.len() - length + 1].iter().max().unwrap();
+    let index = bank.iter().position(|x| x == max).unwrap();
+    *max as u64 * 10_u64.pow(length as u32 - 1) + max_joltage(&bank[index + 1..], length - 1)
+}
+
+pub fn part2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| {
+            max_joltage(
+                &line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec(),
+                12,
+            )
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -38,9 +59,8 @@ mod tests {
         assert_eq!(part1(input()), 17109);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 5782);
+        assert_eq!(part2(input()), 169347417057382);
     }
 }
