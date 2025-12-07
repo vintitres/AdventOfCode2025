51bd0ee3f3fca1a5e3385082af538bea121d17d4
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-07 13:22:04 -0700

    d7p2slow

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub fn part1(input: &str) -> usize {
     let mut beams = HashSet::new();
@@ -26,7 +26,57 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    input.lines().count()
+    input
+        .lines()
+        .enumerate()
+        .fold(
+            HashMap::<usize, HashSet<Vec<usize>>>::new(),
+            |mut beams, (line_num, line)| {
+                if !beams.is_empty() {
+                    let mut new_beams: HashMap<usize, HashSet<Vec<usize>>> = HashMap::new();
+                    for (&i, paths) in beams.iter() {
+                        let new_paths: HashSet<Vec<usize>> = paths
+                            .into_iter()
+                            .map(|path| {
+                                let mut new_path = path.clone();
+                                new_path.push(i);
+                                new_path
+                            })
+                            .collect();
+                        if line.chars().nth(i).unwrap() == '^' {
+                            if i + 1 < line.len() {
+                                new_beams
+                                    .entry(i + 1)
+                                    .or_insert_with(HashSet::new)
+                                    .extend(new_paths.clone());
+                            }
+                            if i > 0 {
+                                new_beams
+                                    .entry(i - 1)
+                                    .or_insert_with(HashSet::new)
+                                    .extend(new_paths);
+                            }
+                        } else {
+                            new_beams
+                                .entry(i)
+                                .or_insert_with(HashSet::new)
+                                .extend(new_paths);
+                        }
+                    }
+                    println!("{:?}", new_beams);
+                    println!("{}", line_num);
+                    return new_beams;
+                } else if let Some(pos) = line.chars().position(|c| c == 'S') {
+                    let mut paths = HashSet::new();
+                    paths.insert(vec![]);
+                    beams.insert(pos, paths);
+                }
+                beams
+            },
+        )
+        .into_values()
+        .map(|paths| paths.len())
+        .sum()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use adventofcode2025::day07 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day7.txt");
+    let input = include_str!("../input/2025/day7e.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
