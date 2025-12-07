8e4a1484d582f0f518bb1fe52950591e5bd686c9
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-06 23:29:57 -0700

    d5p1

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,23 @@
+use itertools::Itertools;
+
 pub fn part1(input: &str) -> usize {
-    input.lines().count()
+    let mut lines = input.lines();
+    let ranges = lines
+        .by_ref()
+        .take_while(|line| !line.trim().is_empty())
+        .map(|line| {
+            let (start, end) = line.split_once('-').unwrap();
+            (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap())
+        })
+        .collect_vec();
+    lines
+        .map(|i| i.parse::<u64>().unwrap())
+        .filter(|&ingredient| {
+            ranges
+                .iter()
+                .any(|&(start, end)| start <= ingredient && ingredient <= end)
+        })
+        .count()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,13 +29,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        include_str!("../input/2025/day2.txt")
+        include_str!("../input/2025/day5.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 720);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2025::day04 as day;
+use adventofcode2025::day05 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day4.txt");
+    let input = include_str!("../input/2025/day5.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
