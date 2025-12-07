45a08cb148b565996acebc8012c7a229daee63a6
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-06 23:14:06 -0700

    d4p2

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 type Pos = (isize, isize);
@@ -33,8 +35,9 @@ impl World {
         self.map[0].len()
     }
 
-    fn check_rolls(&self, pos: Pos) -> bool {
-        self.get(pos) == Some('@')
+    fn check_rolls(&self, pos: Pos, removed: &HashSet<Pos>) -> bool {
+        !removed.contains(&pos)
+            && self.get(pos) == Some('@')
             && vec![
                 (pos.0, pos.1 + 1),
                 (pos.0, pos.1 - 1),
@@ -46,7 +49,7 @@ impl World {
                 (pos.0 - 1, pos.1 + 1),
             ]
             .iter()
-            .filter(|p| self.get(**p) == Some('@'))
+            .filter(|p| !removed.contains(p) && self.get(**p) == Some('@'))
             .count()
                 < 4
     }
@@ -56,12 +59,25 @@ pub fn part1(input: &str) -> usize {
     let w = World::read(input);
     (0..w.height())
         .flat_map(|i| (0..w.width()).map(move |j| (i, j)))
-        .filter(|(i, j)| w.check_rolls((*i as isize, *j as isize)))
+        .filter(|(i, j)| w.check_rolls((*i as isize, *j as isize), &HashSet::new()))
         .count()
 }
 
 pub fn part2(input: &str) -> usize {
-    input.lines().count()
+    let w = World::read(input);
+    let mut removed = HashSet::new();
+    loop {
+        let new_removed = (0..w.height())
+            .flat_map(|i| (0..w.width()).map(move |j| (i as isize, j as isize)))
+            .filter(|(i, j)| w.check_rolls((*i as isize, *j as isize), &removed))
+            .map(|(i, j)| (i as isize, j as isize))
+            .collect_vec();
+        if new_removed.is_empty() {
+            break;
+        }
+        removed.extend(new_removed);
+    }
+    removed.len()
 }
 
 #[cfg(test)]
@@ -77,9 +93,8 @@ mod tests {
         assert_eq!(part1(input()), 1533);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 5782);
+        assert_eq!(part2(input()), 9206);
     }
 }
