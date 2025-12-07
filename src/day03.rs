0311335f2fac992cbf4b550a55ab9e2a6349496c
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-06 22:02:45 -0700

    d3p1

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,24 @@
-pub fn part1(input: &str) -> usize {
-    input.lines().count()
+pub fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .rev()
+                .scan(None, |seen_max, x| {
+                    if let Some(_seen_max) = *seen_max {
+                        let v = x * 10 + _seen_max;
+                        *seen_max = Some(std::cmp::max(_seen_max, x));
+                        Some(v)
+                    } else {
+                        *seen_max = Some(x);
+                        Some(0)
+                    }
+                })
+                .max()
+                .unwrap()
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -14,10 +33,9 @@ mod tests {
         include_str!("../input/2025/day3.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 17109);
     }
 
     #[ignore = "not implemented"]
