97008688f75f0a972a9ae544946d314cf0ee7039
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-07 11:06:48 -0700

    d6p1

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,43 @@
-pub fn part1(input: &str) -> usize {
-    input.lines().count()
+use itertools::Itertools;
+
+pub fn part1(input: &str) -> u64 {
+    let ops = input
+        .lines()
+        .skip_while(|line| line.trim_start().chars().next().unwrap().is_numeric())
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.chars().next().unwrap())
+        .collect_vec();
+    input
+        .lines()
+        .take_while(|line| line.trim_start().chars().next().unwrap().is_numeric())
+        .fold(
+            ops.iter()
+                .map(|&op| match op {
+                    '*' => (1, op),
+                    '+' => (0, op),
+                    _ => panic!("Invalid operator {}", op),
+                })
+                .collect_vec(),
+            |mut acc, line| {
+                acc.iter_mut()
+                    .zip(
+                        line.split_ascii_whitespace()
+                            .map(|s| s.parse::<u64>().unwrap())
+                            .collect_vec(),
+                    )
+                    .for_each(|((a, op), b)| match op {
+                        '*' => *a *= b,
+                        '+' => *a += b,
+                        _ => panic!("Invalid operator"),
+                    });
+                acc
+            },
+        )
+        .iter()
+        .map(|(a, _)| *a)
+        .sum::<u64>()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,13 +49,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        include_str!("../input/2025/day2.txt")
+        include_str!("../input/2025/day6.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 4719804927602);
     }
 
     #[ignore = "not implemented"]
