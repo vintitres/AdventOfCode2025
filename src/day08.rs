4b19c723d0fd5fc375157f0d623df44a5f8318f3
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-07 22:27:57 -0700

    d8p1slow

--- src/day08.rs
+++ src/day08.rs
@@ -1,5 +1,54 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+fn distance3d(a: &(i32, i32, i32), b: &(i32, i32, i32)) -> i64 {
+    ((a.0 - b.0) as i64).abs().pow(2)
+        + ((a.1 - b.1) as i64).abs().pow(2)
+        + ((a.2 - b.2) as i64).abs().pow(2)
+}
+
 pub fn part1(input: &str) -> usize {
-    input.lines().count()
+    let boxes = input
+        .lines()
+        .map(|line| {
+            let (x, y, z) = line
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            (x, y, z)
+        })
+        .collect_vec();
+
+    let mut dist_pairs = Vec::new();
+    for i in 0..boxes.len() {
+        for j in i + 1..boxes.len() {
+            dist_pairs.push((distance3d(&boxes[i], &boxes[j]), (i, j)));
+        }
+    }
+    dist_pairs.sort();
+
+    let mut circuts_sets = (0..boxes.len()).map(|i| HashSet::from([i])).collect_vec();
+    let mut circuts_index = (0..boxes.len()).collect_vec();
+
+    for (_, (mut i, mut j)) in &dist_pairs[0..1000] {
+        if circuts_sets[circuts_index[i]].len() > circuts_sets[circuts_index[j]].len() {
+            std::mem::swap(&mut i, &mut j);
+        }
+        let to_move = circuts_sets[circuts_index[i]].clone();
+        circuts_sets[circuts_index[i]].clear();
+        for k in to_move {
+            circuts_sets[circuts_index[j]].insert(k);
+            circuts_index[k] = circuts_index[j];
+        }
+    }
+    circuts_sets
+        .iter()
+        .map(|set| set.len())
+        .sorted()
+        .rev()
+        .take(3)
+        .product()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -14,10 +63,9 @@ mod tests {
         include_str!("../input/2025/day2.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 46398);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2025::day07 as day;
+use adventofcode2025::day08 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day7.txt");
+    let input = include_str!("../input/2025/day8.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
