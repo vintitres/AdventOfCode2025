a6de1cb4ad99fc7ecc8f22944ed403acd1b42f16
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-09 22:35:01 -0700

    d10p1

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,53 @@
+use std::collections::{HashSet, VecDeque};
+
+use itertools::Itertools;
+
+fn min_push(lights: &u64, buttons: &[u64]) -> usize {
+    let mut queue = VecDeque::from([(*lights, 0)]);
+    let mut visited = HashSet::from([*lights]);
+    while let Some((lights, pushes)) = queue.pop_front() {
+        for button in buttons {
+            let new_lights = lights ^ button;
+            if new_lights == 0 {
+                return pushes + 1;
+            }
+            if visited.insert(new_lights) {
+                queue.push_back((new_lights, pushes + 1));
+            }
+        }
+    }
+    unreachable!()
+}
+
 pub fn part1(input: &str) -> usize {
-    input.lines().count()
+    input
+        .lines()
+        .map(|line| {
+            let (lights, buttons) = line.split_once(' ').unwrap();
+            let buttons = buttons
+                .split(' ')
+                .filter(|&button| button.starts_with('('))
+                .map(|button| button.strip_suffix(')').unwrap().strip_prefix('(').unwrap())
+                .map(|button| {
+                    button
+                        .split(',')
+                        .map(|bit_index| 2_u64.pow(bit_index.parse::<u32>().unwrap()))
+                        .sum::<u64>()
+                })
+                .collect_vec();
+            let lights = lights
+                .strip_prefix('[')
+                .unwrap()
+                .strip_suffix(']')
+                .unwrap()
+                .chars()
+                .enumerate()
+                .filter(|&(_, c)| c == '#')
+                .map(|(i, _)| 2_u64.pow(i as u32))
+                .sum::<u64>();
+            min_push(&lights, &buttons)
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,13 +59,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        include_str!("../input/2025/day2.txt")
+        include_str!("../input/2025/day10.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 962);
+        assert_eq!(part1(input()), 507);
     }
 
     #[ignore = "not implemented"]
