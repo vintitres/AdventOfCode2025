6e05dcf108916d75415e9ef39ce068794c037e11
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-11 00:06:32 -0700

    d10/

--- src/day10.rs
+++ src/day10.rs
@@ -50,28 +50,43 @@ pub fn part1(input: &str) -> usize {
         .sum()
 }
 
-fn min_push_joltage(_joltage: &[usize], _buttons: &[Vec<usize>]) -> u64 {
-    1
-    /*
-    * [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-
-    *
-    *    3 5 4 7
-    *    -------
-    *    0 0 0 1
-    *    0 1 0 1
-    *    0 0 1 0
-    *    0 0 1 1
-    *    1 0 1 0
-    *    1 1 0 0
-    *
-    */
+fn min_push_joltage(
+    needed_joltage: &[usize],
+    buttons: &Vec<&Vec<usize>>,
+    buttons_start: usize,
+) -> Option<u64> {
+    if needed_joltage.iter().all(|&j| j == 0) {
+        return Some(0);
+    }
+    buttons
+        .iter()
+        .skip(buttons_start)
+        .map(|button| {
+            if button.iter().any(|&i| needed_joltage[i] == 0) {
+                return None;
+            }
+            let new_needed_joltage = needed_joltage
+                .iter()
+                .enumerate()
+                .map(|(i, &j)| j - if button.contains(&i) { 1 } else { 0 })
+                .collect_vec();
+            let mpj1 = match min_push_joltage(&new_needed_joltage, buttons, buttons_start) {
+                Some(j) => Some(j + 1),
+                None => None,
+            };
+            let mpj2 = match min_push_joltage(&new_needed_joltage, buttons, buttons_start + 1) {
+                Some(j) => Some(j + 1),
+                None => None,
+            };
+            vec![mpj1, mpj2].into_iter().flatten().min()
+        })
+        .flatten()
+        .min()
 }
 
 pub fn part2(input: &str) -> u64 {
     input
         .lines()
-        .take(1)
         .map(|line| {
             let buttons = line
                 .split(' ')
@@ -86,8 +101,24 @@ pub fn part2(input: &str) -> u64 {
                 .collect_vec();
 
             let (buttons, joltage) = buttons.split_at(buttons.len() - 1);
-            min_push_joltage(&joltage[0], &buttons)
+            let joltage = &joltage[0];
+            let mpl = min_push_joltage(
+                &joltage,
+                &buttons
+                    .iter()
+                    .sorted_by_key(|button| {
+                        (
+                            button.iter().map(|&i| joltage[i]).min().unwrap(),
+                            10 - button.len(),
+                        )
+                    })
+                    .collect_vec(),
+                0,
+            );
+            println!("{:?}", mpl);
+            mpl
         })
+        .flatten()
         .sum()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2025::day11 as day;
+use adventofcode2025::day10 as day;
 
 fn main() {
-    let input = include_str!("../input/2025/day11.txt");
+    let input = include_str!("../input/2025/day10e.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
