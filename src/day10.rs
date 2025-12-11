cc3aea6d868567e401d61148f4ed0e0a33fb1d57
vintitres <725159+vintitres@users.noreply.github.com>
2025-12-11 01:14:00 -0700

    d10?

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use itertools::Itertools;
 
@@ -52,36 +52,61 @@ pub fn part1(input: &str) -> usize {
 
 fn min_push_joltage(
     needed_joltage: &[usize],
-    buttons: &Vec<&Vec<usize>>,
-    buttons_start: usize,
+    buttons: &Vec<Vec<usize>>,
+    mem: &mut HashMap<(Vec<usize>, Vec<Vec<usize>>), u64>,
 ) -> Option<u64> {
+    if let Some(&result) = mem.get(&(needed_joltage.to_vec(), buttons.to_vec())) {
+        return Some(result);
+    }
     if needed_joltage.iter().all(|&j| j == 0) {
         return Some(0);
     }
-    buttons
+    let (i, &j) = needed_joltage
         .iter()
-        .skip(buttons_start)
-        .map(|button| {
-            if button.iter().any(|&i| needed_joltage[i] == 0) {
-                return None;
-            }
-            let new_needed_joltage = needed_joltage
-                .iter()
-                .enumerate()
-                .map(|(i, &j)| j - if button.contains(&i) { 1 } else { 0 })
-                .collect_vec();
-            let mpj1 = match min_push_joltage(&new_needed_joltage, buttons, buttons_start) {
-                Some(j) => Some(j + 1),
-                None => None,
-            };
-            let mpj2 = match min_push_joltage(&new_needed_joltage, buttons, buttons_start + 1) {
-                Some(j) => Some(j + 1),
-                None => None,
-            };
-            vec![mpj1, mpj2].into_iter().flatten().min()
-        })
-        .flatten()
-        .min()
+        .enumerate()
+        .filter(|&(_, &j)| j > 0)
+        .min_by_key(|&(_, j)| (j, buttons.iter().filter(|b| b.contains(&j)).count()))
+        .unwrap();
+    if let Some((bi, b)) = buttons
+        .iter()
+        .enumerate()
+        .filter(|(_, b)| b.contains(&i))
+        .next()
+    {
+        let mut new_buttons = buttons.clone();
+        new_buttons.remove(bi);
+        let res = (0..=j)
+            .map(|bp| {
+                // println!(
+                //     "needed_joltage: {:?}, buttons: {:?}, i: {}, j: {}, bi: {}, b: {:?}, bp: {}",
+                //     needed_joltage, buttons, i, j, bi, b, bp
+                // );
+                if needed_joltage
+                    .iter()
+                    .enumerate()
+                    .any(|(i, &j)| b.contains(&i) && j < bp)
+                {
+                    return None;
+                }
+                let new_needed_joltage = needed_joltage
+                    .iter()
+                    .enumerate()
+                    .map(|(i, &j)| j - if b.contains(&i) { bp } else { 0 })
+                    .collect_vec();
+                match min_push_joltage(&new_needed_joltage, &new_buttons, mem) {
+                    Some(min_joltage) => Some(bp as u64 + min_joltage),
+                    None => None,
+                }
+            })
+            .flatten()
+            .min();
+        if let Some(res) = res {
+            mem.insert((needed_joltage.to_vec(), buttons.to_vec()), res);
+        }
+        res
+    } else {
+        None
+    }
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -102,19 +127,7 @@ pub fn part2(input: &str) -> u64 {
 
             let (buttons, joltage) = buttons.split_at(buttons.len() - 1);
             let joltage = &joltage[0];
-            let mpl = min_push_joltage(
-                &joltage,
-                &buttons
-                    .iter()
-                    .sorted_by_key(|button| {
-                        (
-                            button.iter().map(|&i| joltage[i]).min().unwrap(),
-                            10 - button.len(),
-                        )
-                    })
-                    .collect_vec(),
-                0,
-            );
+            let mpl = min_push_joltage(&joltage, &buttons.to_vec(), &mut HashMap::new());
             println!("{:?}", mpl);
             mpl
         })
